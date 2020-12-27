72ba37f59e414fdd2e6416a9c9fe2aa3e45be46f
Radu <radu.deleanu14@gmail.com>
2020-12-27 20:30:24 +0200

    Made the server truly concurrent, added first response to certain commands

--- server.c
+++ server.c
@@ -27,13 +27,14 @@ int callback(void *str, int argc, char **argv, char **azColName)
 }
 
 //Command evaluation and database access
-int commandEval(int fd)
+int commandEval(int fd, fd_set *actfds)
 {
 	//Server
 	char send[max]; 				//mesajul trimis inapoi de server catre client
 	char recv[max]=" ";				//mesajul primit de server de la client
 	char msg[max];					//buffer pentru construirea mesajului
-	int isAdmin;
+	int isAdmin=0;
+	int isLoggedIn=0;
 
 	//Database
 	char *sql[max];
@@ -48,86 +49,93 @@ int commandEval(int fd)
 		sqlite3_close(db);
 		return 1;
 	}
+	fflush(stdout);
+	memset (recv, 0, sizeof (recv));
+	memset (send, 0, sizeof (send));
+	memset (msg, 0, sizeof (msg));
 
-	while(1)
+	if (read (fd, recv, sizeof (recv)) < 0)
 	{
-		fflush(stdout);
-		memset (recv, 0, sizeof (recv));
-		memset (send, 0, sizeof (send));
-		memset (msg, 0, sizeof (msg));
+        perror ("[server]Eroare la read () de la client.\n");
+        return 1;
+    }
+    //printf("received %s from client", recv);
 
-		isAdmin=0;
+    if (strstr (recv, "login ") != NULL)
+    {
 
-		if (read (fd, recv, sizeof (recv)) <= 0)
-		{
-            perror ("[server]Eroare la read () de la client.\n");
-            return 1;
-        }
-        printf("received %s from client", recv);
+    }
+    else if (strstr (recv, "register ") != NULL)
+    {
 
-        if (strstr (recv, "login ") != NULL)
-        {
+    }
+    else if ((strstr (recv, "delete song ") != NULL) && isAdmin==1)
+    {
 
-        }
-        else if (strstr (recv, "register ") != NULL)
-        {
+    }
+    else if ((strstr (recv, "restrict vote ") != NULL) && isAdmin==1)
+    {
 
-        }
-        else if (strstr (recv, "delete song ") != NULL)
-        {
+    }
+    else if (strstr (recv, "add song ") != NULL)
+    {
 
-        }
-        else if (strstr (recv, "restrict vote ") != NULL)
-        {
+    }
+    else if (strstr (recv, "vote song ") != NULL)
+    {
 
-        }
-        else if (strstr (recv, "add song ") != NULL)
-        {
+    }
+    else if (strstr (recv, "comment song ") != NULL)
+    {
 
-        }
-        else if (strstr (recv, "vote song ") != NULL)
-        {
+    }
+    else if (strstr (recv, "top song general ") != NULL)
+    {
 
-        }
-        else if (strstr (recv, "comment song ") != NULL)
-        {
+    }
+    else if (strstr (recv, "top song genre ") != NULL)
+    {
 
-        }
-        else if (strstr (recv, "top song general ") != NULL)
-        {
+    }
+    else if (strstr (recv, "commands") != NULL)
+    {
+    	strcpy(msg,"The list of available commands, followed by the necessary parameters:\n1. login 'username' 'password'\n2. register 'user/admin' 'username' 'password'\n");
 
-        }
-        else if (strstr (recv, "top song genre ") != NULL)
-        {
+    	if(isLoggedIn)
+    		strcpy(msg,"3. add song 'song_name' 'description' 'genre' 'link'\n4. vote song 'song_name'\n5. comment song 'song_name' 'comment'\n6. top song general\n7. top song 'genre'\n");
 
-        }
-        else if (strstr (recv, "commands") != NULL)
-        {
+    	if(isAdmin)
+    		strcpy(msg,"8. delete song 'song_name'\n9. restrict vote 'username'\n");
+    	strcpy(send,msg);
 
-        }
-        else if (strstr (recv, "quit") != NULL)
-        {
-        	strcpy(send, "quit");
-        }
+    }
+    else if (strstr (recv, "quit") != NULL)
+    {
+    	strcpy(send, "quit");
+    	//quit=1;
+    }
+    else
+    {
+    	strcpy(send, "command not recognized! For a list of available commands, try 'commands'");
+    }
 
-        int sendLength=strlen(send);
+    int sendLength=strlen(send);
 
-        if (write (fd, send, sendLength) <= 0)
-        {
-			perror ("[server]Eroare la write() catre client.\n");
-            continue;
-     	}
+    if (write (fd, send, sendLength) <= 0)
+    {
+		perror ("[server]Eroare la write() catre client.\n");
+        //continue;
+ 	}
 
-        printf("sent %s to client", recv);
-        fflush(stdout);
+ 	if(strncmp(recv, "quit", 4)==0)
+ 	{
+ 		close (fd);					//inchidem conexiunea cu clientul
+ 		FD_CLR (fd, actfds);		
+ 	}
 
-     	if(strcmp(send,"quit"))
-     	{
-     		close(fd);
-     		break;
-     	}
-
-	}
+    //printf("sent %s to client", recv);
+    //fflush(stdout);
+	sqlite3_close(db);
 	return 0;
 }
 
@@ -137,7 +145,7 @@ int main ()
 	//Server
 	struct sockaddr_in server;		//structurile pentru server si clienti
 	struct sockaddr_in from;		
-	int optval=1;					//optiune folosita pentru setsockopt()
+	const int optval=1;					//optiune folosita pentru setsockopt()
 	int sd, client;					//descriptori de socket
 	int fd;							//descriptor folosit pentru parcurgerea listelor de descriptori
 	int nfds;						//numarul maxim de descriptori
@@ -150,8 +158,6 @@ int main ()
   		perror ("[server]Eroare la socket().\n");
     	return errno;
   	}
-
-  	printf("%d\n",sd);
 
   	//setam pentru socket optiunea SO_REUSEADDR
 	setsockopt(sd, SOL_SOCKET, SO_REUSEADDR,&optval,sizeof(optval));
@@ -213,17 +219,15 @@ int main ()
 		}
 	  	for (fd = 0; fd <= nfds; fd++)			//parcurgem multimea de descriptori
 		{
-			printf("calling for");
 	  		if (fd != sd && FD_ISSET (fd, &readfds)) 	  	//este un socket de citire pregatit?
 	    	{	
-	      		if (commandEval(fd))
+	      		if (commandEval(fd,&actfds))
 				{
 		  			printf ("[server] S-a deconectat clientul cu descriptorul %d.\n",fd);
 		  			fflush (stdout);
-		  			close (fd);					//inchidem conexiunea cu clientul
-		  			FD_CLR (fd, &actfds);		//scoatem si din multime
-		  		}
+		  		}	
 	    	}
 		}
   	}
+  	printf("bag pl plec");
 }
