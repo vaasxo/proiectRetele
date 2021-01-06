bae1ed31d4e46b9a89b2a5d7c7195cd90e2b0924
Radu <radu.deleanu14@gmail.com>
2021-01-06 20:50:40 +0200

    general appearance

--- server.c
+++ server.c
@@ -260,64 +260,73 @@ int commandEval(int fd)
 	return 0;
 }*/
 
-int main() {
-    //Server
-    struct sockaddr_in server, from;        //structurile pentru server si clienti
-    int sd, client;                         //descriptori de socket
-    pid_t pid;
-    unsigned int fromLen = sizeof(from);
+int main ()
+{
+	//Server
+	struct sockaddr_in server, from;		//structurile pentru server si clienti
+	int sd, client;					        //descriptori de socket
+	pid_t pid;
+	unsigned int fromLen = sizeof(from);
 
-    if ((sd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
-        perror("[server]Eroare la socket().\n");
-        return errno;
-    }
+	if ((sd = socket (AF_INET, SOCK_STREAM, 0)) == -1)
+  	{
+  		perror ("[server]Eroare la socket().\n");
+    	return errno;
+  	}
 
-    fromLen = sizeof(from);
-    bzero(&from, sizeof(from));
-    bzero(&server, sizeof(server));
+  	fromLen = sizeof(from);
+  	bzero (&from, sizeof (from));
+  	bzero (&server, sizeof (server));
 
-    server.sin_family = AF_INET;
-    server.sin_addr.s_addr = htonl(INADDR_ANY);
-    server.sin_port = htons(PORT);
+  	server.sin_family = AF_INET;
+  	server.sin_addr.s_addr = htonl (INADDR_ANY);
+  	server.sin_port = htons (PORT);
 
-    //Socket bind
-    if (bind(sd, (struct sockaddr *) &server, sizeof(struct sockaddr)) == -1) {
-        perror("[server]Eroare la bind().\n");
-        return errno;
-    }
+  	//Socket bind
+  	if (bind (sd, (struct sockaddr *) &server, sizeof (struct sockaddr)) == -1)
+  	{
+    	perror ("[server]Eroare la bind().\n");
+    	return errno;
+  	}
 
-    //Start Listening
-    if (listen(sd, 5) == -1) {
-        perror("[server]Eroare la listen().\n");
-        return errno;
-    }
+  	//Start Listening
+  	if (listen (sd, 5) == -1)
+  	{
+    	perror ("[server]Eroare la listen().\n");
+    	return errno;
+  	}
 
-    while (1) {
-        client = accept(sd, (struct sockaddr *) &from, &fromLen);
+  	while(1)
+  	{
+        client = accept (sd, (struct sockaddr *) &from, &fromLen);		//accept
 
-        if (client < 0) {
-            perror("[server] Eroare la accept().\n");
-            continue;
-        }
+		if (client < 0)
+	    {
+	      	perror ("[server] Eroare la accept().\n");
+	      	continue;
+	    }
 
-        pid = fork();
+	    pid=fork();
 
-        if (pid < 0) {
-            perror("[server] Eroare la fork().\n");
-            continue;
-        }
+		if (pid < 0)
+	    {
+	      	perror ("[server] Eroare la fork().\n");
+	      	continue;
+	    }
 
-        if (pid == 0) {
-            close(sd);
+	    if(pid==0)
+	    {
+	    	close(sd);
 
-            if (commandEval(client)) {
-                printf("[server] S-a deconectat clientul cu descriptorul %d.\n", client);
-                fflush(stdout);
-            }
+	    	if (commandEval(client))
+			{
+	  			printf ("[server] S-a deconectat clientul cu descriptorul %d.\n",client);
+	  			fflush (stdout);
+	  		}
 
-            close(client);
-            exit(0);
-        }
-        close(client);
-    }
+	  		close(client);
+	  		exit(0);
+	    }
+	    close(client);
+	}
 }
